b5b393ced0446788a9918daa17b2b30a1c6ef2ff
Kirill Kudryashov <b1nc0d3.kka@gmail.com>
2023-07-20 20:29:55 +0300

    linter changes

--- src/interfaces/bookService.ts
+++ src/interfaces/bookService.ts
@@ -2,9 +2,14 @@ import {User, Item} from "@prisma/client";
 
 export interface IBookService<D, T extends Item, U extends User> {
     db: D
+
     getAllBooks(): Promise<T[]>
+
     createBook(name: string, author: string, user: U): Promise<T>
+
     updateBook(id: number, updated_data: object, user: U): Promise<T>
+
     deleteBook(id: number, user: U): void
+
     getBooksWithFilters(filter_word: string): Promise<T[]>
 }
